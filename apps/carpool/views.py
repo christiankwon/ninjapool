fafbdde6dee9680f1921b64d655107d10d125186
Christian Kwon <christiankwon@outlook.com>
2016-09-28 16:06:59 -0700

    add carpool app

--- apps/carpool/views.py
+++ apps/carpool/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ..account.models import User
 
 # Create your views here.
+def index(request):
+    return render(request, 'carpool/index.html')
