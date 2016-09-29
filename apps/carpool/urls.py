2f9d823d167ba7d644f9e9fd4b674c8a8cae5222
Paul Mun <paulmun818@gmail.com>
2016-09-29 08:57:03 -0700

    Fixed indentation errors

--- apps/carpool/urls.py
+++ apps/carpool/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^dashboard$', views.dashboard, name='dashboard'),
     url(r'^checkin/(?P<checkinid>[0-9]+$)', views.checkin, name='checkin'),
     url(r'^register$', views.register, name='register'),
 ]
--- apps/carpool/views.py
+++ apps/carpool/views.py
@@ -7,14 +7,14 @@ def index(request):
     return render(request, 'carpool/index.html')
 
 def dashboard(request):
-	user = User.objects.get(id=request.session['user_id'])
+    user = User.objects.get(id=request.session['user_id'])
     stops = User.objects.filter(carpool_id=user.carpool_id)
     carpool = models.Carpool.objects.get(id=user.carpool_id)
     context = {'user':user, 'stops':stops, 'carpool':carpool}
     return render(request, 'carpool/index.html', context)
 
 def checkin(request, checkinid):
-	return redirect(reverse('carpool:index'))
+    return redirect(reverse('carpool:index'))
 
 def register(request):
     return render(request, 'carpool/register.html')
