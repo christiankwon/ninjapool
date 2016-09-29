11e946b067eced46a44a3d5193b42fce9f0a40d3
Christian Kwon <christiankwon@outlook.com>
2016-09-29 09:10:13 -0700

    fix carpool index

--- apps/carpool/views.py
+++ apps/carpool/views.py
@@ -6,13 +6,12 @@ from ..account.models import User
 
 # Create your views here.
 def index(request):
-    return render(request, 'carpool/index.html')
-
-def dashboard(request):
     user = User.objects.get(id=request.session['user_id'])
     stops = User.objects.filter(carpool_id=user.carpool_id)
     carpool = models.Carpool.objects.get(id=user.carpool_id)
+
     context = {'user':user, 'stops':stops, 'carpool':carpool}
+
     return render(request, 'carpool/index.html', context)
 
 
