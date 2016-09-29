9bc683c5a9343046e17b9760859f8d5381b7e544
Paul Mun <paulmun818@gmail.com>
2016-09-29 07:56:59 -0700

    Fixed syntax in carpools

--- apps/carpool/views.py
+++ apps/carpool/views.py
@@ -4,7 +4,7 @@ from ..account.models import User
 
 # Create your views here.
 def index(request):
-    user = User.objects.get(id=request.session['user'])
+    user = User.objects.get(id=request.session['user_id'])
     stops = User.objects.filter(carpool_id=user.carpool_id)
     carpool = models.Carpool.get(id=user.carpool_id)
     context = {'user':user, 'stops':stops, 'carpool':carpool}
