688885ce04318bb225a17ce63ab3dc1cbc7860e2
Christian Kwon <christiankwon@outlook.com>
2016-09-30 10:22:51 -0700

    Clean up wall app

--- apps/wall/models.py
+++ apps/wall/models.py
@@ -3,24 +3,23 @@ from ..account.models import User
 
 
 class WallManager(models.Manager):
-
     def new_wall(self, user_id, session_id):
-        print("test")
         new_wall = self.create()
         new_wall.users.add(user_id, session_id)
         new_wall.save()
+
         return new_wall
 
     def create_wall(self, user_id, session_id):
         messager = User.objects.get(id=session_id)
         messagee = User.objects.get(id=user_id)
         walls = Wall.objects.filter(users=messager).filter(users=messagee)
+
         for wall in walls:
             if wall.users.count() == 2:
                 return wall
 
         return self.new_wall(messager, messagee)
-
 
 
 class MessageManager(models.Manager):
@@ -35,14 +34,17 @@ class MessageManager(models.Manager):
             wall = Wall.objects.get(id=wall_id)
             message = self.create(message_body=message, messager=messager, walls=wall)
             return (True, message, wall_id)
+
         else:
             return (False, errors)
+
 
 class Wall(models.Model):
     users = models.ManyToManyField(User, related_name="messages")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = WallManager()
+
 
 class Message(models.Model):
     walls = models.ForeignKey(Wall)
--- apps/wall/views.py
+++ apps/wall/views.py
@@ -1,53 +1,43 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
+
 from .models import Wall, Message
 from ..account.models import User
-# Create your views here.
 
 
 def wall(request, id):
-	if 'user_id' not in request.session:
-		return redirect('wall:users_list')
+    if 'user_id' not in request.session:
+        return redirect('wall:users_list')
 
-	user = User.objects.get(id=request.session['user_id'])
-	if not Wall.objects.filter(users__id=user.id).filter(id=id):
-		return redirect('wall:users_list')
+    user = User.objects.get(id=request.session['user_id'])
+    if not Wall.objects.filter(users__id=user.id).filter(id=id):
+        return redirect('wall:users_list')
 
-	wall = Wall.objects.get(id=id)
-	posts = Message.objects.filter(walls=wall)
-	context = {
-		'wall': wall,
-		'posts': posts,
-	}
-	return render(request, 'wall/wall.html', context)
+    wall = Wall.objects.get(id=id)
+    posts = Message.objects.filter(walls=wall)
+    context = {
+        'wall': wall,
+        'posts': posts,
+    }
+
+    return render(request, 'wall/wall.html', context)
 
 
 def post_message(request, id):
+    if request.method == 'POST':
+        response = Message.objects.new_message(request.POST['message_body'], request.session['user_id'], id)
 
-	if request.method == 'POST':
-		response = Message.objects.new_message(request.POST['message_body'], request.session['user_id'], id)
+        if not response[0]:
+            for msg in response[1]:
+                messages.error(request, msg)
 
-		if response[0]:
-			messages.success(request, response[1])
-		else:
-			for msg in response[1]:
-				messages.error(request, msg)
-			return redirect(reverse('wall:wall', kwargs={'id':id}))
+            return redirect(reverse('wall:wall', kwargs={'id':id}))
 
-	return redirect(reverse('wall:wall', kwargs={'id':id}))
+    return redirect(reverse('wall:wall', kwargs={'id':id}))
 
-
-def users_list(request):
-	walls = Wall.objects.all()
-	users = User.objects.exclude(id=request.session['user_id'])
-	context = {
-		"users": users,
-		"walls":walls,
-	}
-	return render(request, 'wall/userMessageTest.html', context)
 
 def wall_create(request, id):
-	wall = Wall.objects.create_wall(id, request.session['user_id'])
+    wall = Wall.objects.create_wall(id, request.session['user_id'])
 
-	return redirect(reverse('wall:wall', kwargs={'id':wall.id}))
+    return redirect(reverse('wall:wall', kwargs={'id':wall.id}))
