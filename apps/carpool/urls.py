3da4a4ac970f591eb39e1fc18f7b34e54e563b46
Christian Kwon <christiankwon@outlook.com>
2016-09-30 10:57:25 -0700

    fix typos

--- apps/carpool/urls.py
+++ apps/carpool/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     url(r'^new_carpool_create$', views.new_carpool_create, name='new_carpool_create'),
     url(r'^nearby$', views.nearby, name='nearby'),
     url(r'^leave$', views.leave, name='leave'),
-    url(r'^view_carpool/(?P<carpool_id>[0-9]+)?', views.view_carpool, name='view_carpool'),
+    url(r'^view_carpool/(?P<carpool_id>[0-9]+)$', views.view_carpool, name='view_carpool'),
 ]
